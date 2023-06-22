c288fa8f76f7b544edd832171ce43589ceccc016
fabuleux366 <abbakikyungu@gmail.com>
2023-06-22 12:34:04 -0400

    companiesCreation End

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import DashboardVendeur from "./components/DashboardVendeur";
 import DashboardMining from "./components/DashboardMining";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Companies from "./components/Companies";
 
 function App() {
   return (
     <div className="container-fluid">
-      <DashboardAdmin />
+      {/* <DashboardAdmin /> */}
       {/* <DashboardVendeur /> */}
       {/* <DashboardMining /> */}
       {/* <Login/> */}
       {/* <Register/> */}
+      <Companies />
     </div>
   );
 }
