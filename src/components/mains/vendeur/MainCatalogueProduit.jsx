0e3a45b6fdcb81b63a3056366b1006f220893f46
MosesK <mosesziongo@gmail.com>
2023-07-19 03:27:10 +0200

    affichage categorie

--- src/components/mains/vendeur/MainCatalogueProduit.jsx
+++ src/components/mains/vendeur/MainCatalogueProduit.jsx
@@ -9,6 +9,7 @@ import BorderColorIcon from '@mui/icons-material/BorderColor';
 
 const MainCatalogueProduit = () => {
   const [produits, setProduits] = useState([]);
+  const [categories, setCategories] = useState([]);
 
   useEffect(() => {
     const fetchProducts = async () => {
@@ -21,10 +22,23 @@ const MainCatalogueProduit = () => {
         );
       }
     };
+
+    const fetchCategories = async ()=>{
+      try {
+        const response = await axios.get("/getAllCategories");
+        setCategories(response?.data);
+      }catch (error){
+        console.log(
+            "Une erreur s'est produite lors de la recuperation des produits"
+        );
+      }
+    }
     fetchProducts();
+    fetchCategories();
   }, []);
 
   console.log(produits);
+  console.log(categories);
 
   return (
     <div>
@@ -82,7 +96,7 @@ const MainCatalogueProduit = () => {
                         data-bs-toggle="tab"
                         data-bs-target="#profile-settings"
                       >
-                        Liste des produits démandées
+                        Statistiques Produits et categories
                       </button>
                     </li>
                   </ul>
@@ -95,7 +109,7 @@ const MainCatalogueProduit = () => {
                         <table className="table table-borderless datatable cotation table-hover">
                           <thead>
                             <tr>
-                              <th scope="col">Produit</th>
+                              <th scope="col">Information sur le produit</th>
                               <th scope="col">Categorie</th>
                               <th scope="col">Date enregistrement</th>
                               <th scope="col">Status</th>
@@ -179,7 +193,59 @@ const MainCatalogueProduit = () => {
                     <div
                       className="tab-pane fade  profile-overview"
                       id="profile-edit"
-                    ></div>
+                    >
+                      <div className="card-body mt-3 ">
+                        <table className="table table-borderless datatable cotation table-hover">
+                          <thead>
+                          <tr>
+                            <th scope="col">illustration de la categorie du produit</th>
+                            <th scope="col">Information Categorie</th>
+                            <th scope="col">Actions</th>
+                          </tr>
+                          </thead>
+                          <tbody>{(categories) && categories.map((categorie)=>(
+                              <tr key={categorie.id}>
+                                <td>
+                                  <div className="row justify-content-center">
+                                    <div className="col-4">
+                                      <Link>
+                                        <Avatar
+                                            alt={categorie.nom_categorie}
+                                            src={categorie.illustration_categorie}
+                                            sx={{ width: 100, height: 100 }}
+                                        />
+                                      </Link>
+                                    </div>
+                                  </div>
+                                </td>
+                                <td>
+                                  <div className="row">
+                                    <div className="col align-self-center mt-2">
+                                        {categorie.nom_categorie}
+                                    </div>
+                                  </div>
+                                </td>
+                                <td>
+                                  <div className="row">
+                                    <div className="col align-self-center mt-2">
+                                      <Link to="">
+                                        <IconButton color="error" aria-label="delete">
+                                          <DeleteIcon />
+                                        </IconButton>
+                                      </Link>
+                                      <Link to="">
+                                        <IconButton color="primary" aria-label="edit">
+                                          <BorderColorIcon />
+                                        </IconButton>
+                                      </Link>
+                                    </div>
+                                  </div>
+                                </td>
+                              </tr>
+                          ))}</tbody>
+                        </table>
+                      </div>
+                    </div>
                     <div
                       className="tab-pane fade pt-3"
                       id="profile-settings"
